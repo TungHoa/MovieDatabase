e0aa7b5b99c28e9e36613ea23ff1c47bc2eece38
HoaTung <hoatung1306@gmail.com>
2021-05-24 11:48:45 +0700

    third commit

--- MovieDatabase/ViewController.swift
+++ MovieDatabase/ViewController.swift
@@ -12,11 +12,13 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        config()
     }
     
     private func config() {
         
     }
 
+    
 }
 
